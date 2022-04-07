99dc5a018e4b0f1eaba750ab5bd5b7555217a7b9
AreyBRSK <areybrsk@gmail.com>
2022-04-07 15:43:33 +0300

    Initial Commit

---
+++ ColorPicker_Lesson_2.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPicker_Lesson_2.6
+//
+//  Created by Андрей Барсук on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
